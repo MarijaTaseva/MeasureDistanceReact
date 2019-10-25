3d577dd2c43c9602ed0a8739bd251a111d6686e0
marija.taseva <marija.taseva@gowi.rs>
2019-10-25 11:22:17 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
 
 function App() {
+  const [data, setData] = useState({})
+  const [firstAirport, setFirstAirport] = useState("")
+  const [secondAirport, setSecondAirport] = useState("")
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  
+  async function fetchData() {
+    setLoading(true)
+    await fetch(`https://localhost:44382/api/MeasureDistance/${firstAirport}/${secondAirport}`)
+      .then(results => results.json())
+      .then(data => {
+        setData(data)
+        setError(false)
+      })
+      .catch(error => {
+        console.log(error)
+        setError(true)
+      })
+    setLoading(false)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form className="App-form">
+          <TextField
+            value={firstAirport}
+            label="First IATA code"
+            placeholder="AMS"
+            margin="normal"
+            variant="outlined"
+            className="App-TextField"
+            onChange={event => setFirstAirport(event.target.value)}
+          />
+          <br />
+          <TextField
+            value={secondAirport}
+            label="Second IATA code"
+            placeholder="SKP"
+            margin="normal"
+            variant="outlined"
+            className="App-TextField"
+            onChange={event => setSecondAirport(event.target.value)}
+          />
+          <br />
+          <Button variant="contained" onClick={fetchData}>Measure</Button>
+        </form>
+        <br />
+        {
+          loading && <div>Measuring distance between "<strong>{firstAirport} and {secondAirport}</strong>"</div>
+        }
+        {
+          (
+            error === true ||
+            typeof data.distance === 'undefined' ||
+            data.firstAirportName === "" ||
+            data.secondAirportName === ""
+          )
+            ?
+            <div><span>Please enter existing IATA codes in the format of 3 letters</span></div>
+            :
+            <div>
+              <label>First Airport: </label><span>{data.firstAirportName}</span><br />
+              <label>Second Airport: </label><span>{data.secondAirportName}</span><br />
+              <label>Distance: </label><span>{Math.round(data.distance)} miles</span><br />
+            </div>
+        }
       </header>
     </div>
-  );
+  )
 }
 
 export default App;
